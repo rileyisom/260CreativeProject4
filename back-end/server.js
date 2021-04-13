2850dc9c7fdaf28d907e994a7248f07662c20569
Riley Isom <rileyisom99@gmail.com>
2021-04-13 00:15:20 -0600

    Version 1.2

--- back-end/server.js
+++ back-end/server.js
@@ -231,4 +231,4 @@ app.post('/api/photos', upload.single('photo'), async (req, res) => {
     });
 });
 
-app.listen(3000, () => console.log('Server listening on port 3000!'));
+app.listen(3002, () => console.log('Server listening on port 3002!'));
